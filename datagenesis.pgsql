7376bfb53108927ccb5f0ef0f3dbecc8f8056995
Pier-Luc Picard <pierluc_picard@yahoo.ca>
2017-02-08 22:05:34 -0500

    Adding the directory to the commit

---
+++ datagenesis.pgsql
@@ -0,0 +1,7 @@
+/*This is an example to try if all work*/
+
+DROP TABLE conversation;
+
+CREATE TABLE conversation(id INTEGER NOT NULL UNIQUE, name VARCHAR(20));
+
+INSERT INTO conversation VALUES (1,'Pier-LucSuper');
---
+++ generator/index.js
@@ -0,0 +1,18 @@
+"use strict";
+const _ = require('lodash');
+const fs = require('fs');
+
+var tables ={};
+
+function cleanUp() {
+    console.log('Cleaning the database');
+
+}
+
+function generate(table,generators,number){
+
+
+
+}
+
+console.log('Set ok')
